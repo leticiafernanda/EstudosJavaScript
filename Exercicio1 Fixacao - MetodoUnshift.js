5ab4e76afeb4faad949780bd1cbf2ef2d28f094c
Letícia <32845746+leticiafernanda@users.noreply.github.com>
2021-01-28 22:14:19 -0300

    Add files via upload

---
+++ Exercicio1 Fixacao - MetodoUnshift.js
@@ -0,0 +1,29 @@
+//Insere os itens no inicio do array: 10,20,30,40,50,60,70,80,90
+var array1 = ['branco', 10, 20, 30];
+array1.unshift(10);
+console.log(array1);
+
+//Insere os itens no inicio do array:
+var array2 = ['branco', 10, 20, 30];
+array2.unshift('roxo',10,'amarelo',450);
+console.log(array2);
+
+//Insere os itens no inicio do array:
+var array3 = [0,2,25, 20, 30];
+array3.unshift('leticia',10,'fernanda',450);
+console.log(array3);
+
+//Insere os itens no inicio do array:
+var array4 = [1, 10, 20, 30];
+array4.unshift(20+90);
+console.log(array4);
+
+//Insere os itens no inicio do array:
+var array5 = ['branco', 10, 20, 30];
+array5.unshift(10-10);
+console.log(array5);
+
+//Insere os itens no inicio do array:
+var array6 = [8];
+array6.unshift('leticia');
+console.log(array6);
