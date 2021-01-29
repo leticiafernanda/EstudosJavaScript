6aaa9f1d700b8c4fdb9d90ba8fa04db181e4245c
Letícia <32845746+leticiafernanda@users.noreply.github.com>
2021-01-28 22:12:20 -0300

    Add files via upload

---
+++ Exercicio1 Fixacao - MetodoPush.js
@@ -0,0 +1,29 @@
+//Acrescente os itens: 10,20,30,40,50,60,70,80,90
+var array1 = ['branco', 10, 20, 30];
+array1.push(10,20,30,40,50,60,70,80,90);
+console.log(array1);
+
+//Acrescente os itens
+var array2 = ['branco', 10, 20, 30];
+array2.push('roxo',10,'amarelo',450);
+console.log(array2);
+
+//Acrescente os itens
+var array3 = [0,2,25, 20, 30];
+array3.push('leticia',10,'fernanda',450);
+console.log(array3);
+
+//Acrescente os itens: 10,20,30,40,50,60,70,80,90
+var array4 = [1, 10, 20, 30];
+array4.push(20+90);
+console.log(array4);
+
+//Acrescente os itens
+var array5 = ['branco', 10, 20, 30];
+array5.push(10-10);
+console.log(array5);
+
+//Acrescente os itens
+var array6 = [8];
+array6.push('leticia');
+console.log(array6);
